0ccc56428f24681a78a146ebdb612dac4422740c
YSc21 <yshinc21@gmail.com>
2016-11-17 22:45:57 -0500

    Changed self.using from dict to list in scanner.py

--- webcocktail/scanner.py
+++ webcocktail/scanner.py
@@ -9,36 +9,40 @@ class Scanner(object):
         self.log = get_log(self.__class__.__name__)
         self.webcocktail = webcocktail
         self.plugins = plugins.all_plugins
-        self.using = dict()
+        self.using = []
         self.log.info('All plugins: %s' % self.plugins)
 
-    def _new_plugin(self, name):
-        self.plugins[name] = self.plugins[name]()
-        self.using[name] = self.plugins[name]
+    def _new_plugin(self, module):
+        plugin = module()
+        name = plugin.__class__.__name__
+
+        self.plugins[name] = plugin
+        index = self.using.index(module)
+        self.using[index] = self.plugins[name]
+        return plugin
 
     def use(self, name):
         if name == 'all':
-            self.using = self.plugins
+            self.using = [self.plugins[name] for name in self.plugins]
         elif name == 'default':
-            items = self.plugins.items()
-            self.using.clear()
-            self.using.update((k, v) for k, v in items if k in Scanner.DEFAULT)
+            self.using = [self.plugins[name] for name in Scanner.DEFAULT]
         elif name in self.plugins:
-            self.using[name] = self.plugins[name]
-        self.log.info('Using plugins: %s' % [p for p in self.using])
+            self.using.append(self.plugins[name])
+        self.log.info('Using plugins: %s' % self.using)
 
     def disuse(self, name):
         if name == 'all':
-            self.using = dict()
+            self.using = []
         elif name in self.plugins:
-            self.using.pop(name)
+            self.using.remove(self.plugins[name])
+            self.log.info('%s is removed' % name)
 
     def scan(self, request):
         results = []
-        for name in self.using:
-            if type(self.using[name]) is type:
-                self._new_plugin(name)
-            results.extend(self.using[name].get_results(request))
+        for plugin in self.using:
+            if type(plugin) is type:
+                plugin = self._new_plugin(plugin)
+            results.extend(plugin.get_results(request))
         return results
 
     def scan_all(self, requests):
